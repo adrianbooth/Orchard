103aedca5a7a3cbd32106c425a7ed216cd4778b2
Adrian Booth <adz001@msn.com>
2016-11-09 20:45:05 +0000

    add basic sitemap service and return xml content from controller

--- src/Orchard.Web/Modules/AdrianBooth.Orchard.Sitemap/Controllers/SitemapController.cs
+++ src/Orchard.Web/Modules/AdrianBooth.Orchard.Sitemap/Controllers/SitemapController.cs
@@ -1,11 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
+using AdrianBooth.Orchard.Sitemap.Services;
 
 namespace AdrianBooth.Orchard.Sitemap.Controllers
 {
     public class SitemapController : Controller
     {
-        public string Index() {
-            return "Sitemap Controller";
+        private readonly ISitemapService _sitemapService;
+
+        public SitemapController(ISitemapService sitemapService)
+        {
+            _sitemapService = sitemapService;
+        }
+        public ContentResult Index()
+        {
+            var sitemapContent = _sitemapService.GetSitemap();
+
+            return new ContentResult
+            {
+                Content = sitemapContent,
+                ContentEncoding = Encoding.UTF8,
+                ContentType = "application/xml"
+            };
         }
     }
 }
---
+++ src/Orchard.Web/Modules/AdrianBooth.Orchard.Sitemap/Services/SitemapService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Orchard.Autoroute.Models;
+using Orchard.Caching;
+using Orchard.ContentManagement;
+using Orchard.Services;
+
+namespace AdrianBooth.Orchard.Sitemap.Services
+{
+    public class SitemapService : ISitemapService
+    {
+        private readonly IContentManager _contentManager;
+        private readonly ICacheManager _cacheManager;
+        private readonly IClock _clock;
+
+        public SitemapService(IContentManager contentManager, ICacheManager cacheManager, IClock clock)
+        {
+            _contentManager = contentManager;
+            _cacheManager = cacheManager;
+            _clock = clock;
+        }
+        public string GetSitemap()
+        {
+            return _cacheManager.Get("simple-sitemap", true, ctx =>
+            {
+                ctx.Monitor(_clock.When(TimeSpan.FromMinutes(60 * 24)));
+                return SitemapString();
+            });
+        }
+
+        private string SitemapString()
+        {
+            var itemLocations = _contentManager.Query<AutoroutePart, AutoroutePartRecord>()
+                  .Where(part => part.DisplayAlias != null).List().Select(e => e.DisplayAlias);
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            stringBuilder.AppendLine("<urlset xmlns=\"http://www.google.com/schemas/sitemap/0.90\">");
+            foreach (var url in itemLocations)
+            {
+                stringBuilder.AppendLine($"<url><loc>{url}</loc></url>");
+            }
+            stringBuilder.AppendLine("</urlset>");
+
+            return stringBuilder.ToString();
+        }
+    }
+}

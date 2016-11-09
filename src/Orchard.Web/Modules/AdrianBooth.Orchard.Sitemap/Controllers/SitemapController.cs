231e24b74ce0fba1b3fed239c145fd75cedc4b16
Adrian Booth <adz001@msn.com>
2016-11-09 19:40:07 +0000

    add controller and route information

---
+++ src/Orchard.Web/Modules/AdrianBooth.Orchard.Sitemap/Controllers/SitemapController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace AdrianBooth.Orchard.Sitemap.Controllers
+{
+    public class SitemapController : Controller
+    {
+        public string Index() {
+            return "Sitemap Controller";
+        }
+    }
+}
